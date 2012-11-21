6683f996f0a62a2b4c0621a7745787514d23fd58
Juan Camilo Hincapie Giraldo <juan.hincapie.is@gmail.com>
2012-11-20 23:14:18 -0500

    Arboles Funcionando Paralelamente

--- Comander/src/Comander.java
+++ Comander/src/Comander.java
@@ -8,45 +8,72 @@ import java.io.InputStreamReader;
 import java.util.Vector;
 
 public class Comander {
-    private JTextField textField1;
+    private JTextField txtDest;
     private JTextPane textPane1;
     private JPanel panel1;
     private JTree trOrig;
-    private JTree tree2;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JTextField textField2;
-    DefaultMutableTreeNode Root;
-    Vector<String> ListaDirSup = new Vector<String>();
-    Vector<String> ListaArchSup = new Vector<String>();
+    private JTree trDest;
+    private JButton copiarButton;
+    private JButton moverButton;
+    private JButton borrarButton;
+    private JButton renombrarButton;
+    private JTextField txtOrig;
+    DefaultMutableTreeNode RootOr;
+    DefaultMutableTreeNode RootDe;
+    Vector<String> ListaDirSupOr = new Vector<String>();
+    Vector<String> ListaArchSupOr = new Vector<String>();
+    Vector<String> ListaDirSupDe = new Vector<String>();
+    Vector<String> ListaArchSupDe = new Vector<String>();
     String localDir;
+    String Ruta;
+
 
     public Comander() {
-        Root = new DefaultMutableTreeNode("Raíz");
-        addHijos(Root);
+        RootOr = new DefaultMutableTreeNode("Raíz");
+        RootDe = new DefaultMutableTreeNode("Raíz");
+        addHijos(RootOr, "O");
+        addHijos(RootDe, "D");
+        try {
+            Process dir = Runtime.getRuntime().exec("cmd /c cd");
+            BufferedReader br = new BufferedReader(new InputStreamReader(dir.getInputStream()));
+            localDir = br.readLine();
+        } catch (Exception e) {
+        }
         $$$setupUI$$$();
         trOrig.addTreeSelectionListener(new TreeSelectionListener() {
             @Override
             public void valueChanged(TreeSelectionEvent e) {
                 DefaultMutableTreeNode seleccionado = (DefaultMutableTreeNode) trOrig.getLastSelectedPathComponent();
-                if (ListaDirSup.contains(seleccionado.getUserObject().toString())) {
-                    addHijos(seleccionado);
+                if (ListaDirSupOr.contains(seleccionado.getUserObject().toString())) {
+                    addHijos(seleccionado, "O");
+                } else if (ListaArchSupOr.contains(seleccionado.getUserObject().toString())) {
+                    Ruta += "\\" + seleccionado.getUserObject().toString();
                 }
+                txtOrig.setText(localDir + "\\" + Ruta);
+            }
+        });
+        trDest.addTreeSelectionListener(new TreeSelectionListener() {
+            @Override
+            public void valueChanged(TreeSelectionEvent e) {
+                DefaultMutableTreeNode seleccionado = (DefaultMutableTreeNode) trDest.getLastSelectedPathComponent();
+                if (ListaDirSupDe.contains(seleccionado.getUserObject().toString())) {
+                    addHijos(seleccionado, "D");
+                } else if (ListaArchSupDe.contains(seleccionado.getUserObject().toString())) {
+                    Ruta += "\\" + seleccionado.getUserObject().toString();
+                }
+                txtDest.setText(localDir + "\\" + Ruta);
             }
         });
     }
 
-    public void addHijos(DefaultMutableTreeNode nodo) {
+    public void addHijos(DefaultMutableTreeNode nodo, String origen) {
         if (nodo.getChildCount() != 0) {
             nodo.removeAllChildren();
         }
         Vector<String> ListaDir = new Vector<String>();
         Vector<String> ListaArch = new Vector<String>();
         DefaultMutableTreeNode upNode = (nodo.isRoot()) ? nodo : (DefaultMutableTreeNode) nodo.getParent();
-        String Ruta = nodo.getUserObject().toString();
+        Ruta = nodo.getUserObject().toString();
         while (!upNode.isRoot()) {
             Ruta = upNode.getUserObject().toString() + "\\" + Ruta;
             upNode = (DefaultMutableTreeNode) upNode.getParent();
@@ -57,11 +84,9 @@ public class Comander {
             BufferedReader br;
             String s = "";
             String strCmd = "cmd /c cd " + localDir + "\\" + Ruta + " & dir";
-            System.out.println(strCmd);
             sysCall = Runtime.getRuntime().exec(strCmd);
             br = new BufferedReader(new InputStreamReader(sysCall.getInputStream()));
             while ((s = br.readLine()) != null) {
-                System.out.println(s);
                 int indexArch = "                                         ".length();
                 if (s.contains("<DIR>")) {
                     int indexDir = s.indexOf("<DIR>") + "<DIR>".length() + 1;
@@ -77,20 +102,28 @@ public class Comander {
             int i;
             for (i = 0; i < ListaDir.size(); i++) {
                 DefaultMutableTreeNode hijo = new DefaultMutableTreeNode(ListaDir.get(i).toString());
+                hijo.setAllowsChildren(true);
+
                 nodo.add(hijo);
             }
             for (i = 0; i < ListaArch.size(); i++) {
                 DefaultMutableTreeNode hijo = new DefaultMutableTreeNode(ListaArch.get(i).toString());
                 nodo.add(hijo);
             }
-            ListaArchSup.addAll(ListaArch);
-            ListaDirSup.addAll(ListaDir);
+            if (origen.equals("O")) {
+                ListaArchSupOr.addAll(ListaArch);
+                ListaDirSupOr.addAll(ListaDir);
+            } else {
+                ListaArchSupDe.addAll(ListaArch);
+                ListaDirSupDe.addAll(ListaDir);
+            }
         } catch (Exception e) {
         }
     }
 
     private void createUIComponents() {
-        trOrig = new JTree(Root);
+        trOrig = new JTree(RootOr);
+        trDest = new JTree(RootDe);
     }
 
     public static void main(String[] args) {
@@ -113,25 +146,21 @@ public class Comander {
         panel1 = new JPanel();
         panel1.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(4, 3, new Insets(0, 0, 0, 0), -1, -1));
         panel1.add(trOrig, new com.intellij.uiDesigner.core.GridConstraints(3, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(150, 50), null, 0, false));
-        tree2 = new JTree();
-        panel1.add(tree2, new com.intellij.uiDesigner.core.GridConstraints(3, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(150, 50), null, 0, false));
+        panel1.add(trDest, new com.intellij.uiDesigner.core.GridConstraints(3, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(150, 50), null, 0, false));
         final JToolBar toolBar1 = new JToolBar();
         panel1.add(toolBar1, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 3, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(-1, 20), null, 0, false));
-        button1 = new JButton();
-        button1.setText("Button");
-        toolBar1.add(button1);
-        button2 = new JButton();
-        button2.setText("Button");
-        toolBar1.add(button2);
-        button3 = new JButton();
-        button3.setText("Button");
-        toolBar1.add(button3);
-        button4 = new JButton();
-        button4.setText("Button");
-        toolBar1.add(button4);
-        button5 = new JButton();
-        button5.setText("Button");
-        toolBar1.add(button5);
+        copiarButton = new JButton();
+        copiarButton.setText("Copiar");
+        toolBar1.add(copiarButton);
+        moverButton = new JButton();
+        moverButton.setText("Mover");
+        toolBar1.add(moverButton);
+        borrarButton = new JButton();
+        borrarButton.setText("Borrar");
+        toolBar1.add(borrarButton);
+        renombrarButton = new JButton();
+        renombrarButton.setText("Renombrar");
+        toolBar1.add(renombrarButton);
         final JLabel label1 = new JLabel();
         label1.setText("Directorio Origen");
         panel1.add(label1, new com.intellij.uiDesigner.core.GridConstraints(1, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
@@ -141,10 +170,10 @@ public class Comander {
         final JLabel label3 = new JLabel();
         label3.setText(">>");
         panel1.add(label3, new com.intellij.uiDesigner.core.GridConstraints(1, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        textField1 = new JTextField();
-        panel1.add(textField1, new com.intellij.uiDesigner.core.GridConstraints(2, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-        textField2 = new JTextField();
-        panel1.add(textField2, new com.intellij.uiDesigner.core.GridConstraints(2, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        txtDest = new JTextField();
+        panel1.add(txtDest, new com.intellij.uiDesigner.core.GridConstraints(2, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        txtOrig = new JTextField();
+        panel1.add(txtOrig, new com.intellij.uiDesigner.core.GridConstraints(2, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
     }
 
     /**
